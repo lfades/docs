fdb48c31cc33ada58dbf36bcdc4254c2fcadbdd6
Luis Alvarez <luis@zeit.co>
2019-08-30 22:08:12 -0500

    Simplified next config

--- next.config.js
+++ next.config.js
@@ -1,56 +1,41 @@
-const {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD
-} = require('next/constants')
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = phase => {
-  const isProdBuild = phase === PHASE_PRODUCTION_BUILD
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER
-
-  const nextConfig = {
-    target: 'serverless',
-
-    exportTrailingSlash: true,
-
-    experimental: {
-      publicDirectory: true
-    },
-
-    // Allow mdx and md files to be pages
-    pageExtensions: ['jsx', 'js', 'mdx', 'md'],
-
-    assetPrefix: isProdBuild || phase === 'phase-export' ? '/docs' : '',
-
-    env: {
-      VERSION: require('./package.json').version,
-      API_URL: process.env.API_URL,
-      IMAGE_ASSETS_URL: 'https://assets.zeit.co/image/upload/front',
-      VIDEO_ASSETS_URL: 'https://assets.zeit.co/video/upload/front',
-      RAW_ASSETS_URL: 'https://assets.zeit.co/raw/upload/front',
-      ASSETS: isProdBuild ? '/docs/static' : '/static'
-    },
-
-    webpack(config, { isServer }) {
-      if (isServer) {
-        require('./scripts/generate-site-map')()
-      }
-      return config
-    }
+const rehypePrism = require('@mapbox/rehype-prism')
+// Adds github.com/mdx-js/mdx to Next.js
+const withMDX = require('@next/mdx')({
+  extension: /\.(md|mdx)?$/,
+  options: {
+    hastPlugins: [rehypePrism]
   }
+})
 
-  // This makes sure we only require build-time plugins at build time
-  if (!isDev && !isProdBuild) {
-    return nextConfig
-  }
+module.exports = withMDX({
+  target: 'serverless',
 
-  const rehypePrism = require('@mapbox/rehype-prism')
-  // Adds github.com/mdx-js/mdx to Next.js
-  const withMDX = require('@next/mdx')({
-    extension: /\.(md|mdx)?$/,
-    options: {
-      hastPlugins: [rehypePrism]
+  exportTrailingSlash: true,
+
+  experimental: {
+    publicDirectory: true
+  },
+
+  // Allow mdx and md files to be pages
+  pageExtensions: ['jsx', 'js', 'mdx', 'md'],
+
+  assetPrefix: isProd ? '/docs' : '',
+
+  env: {
+    VERSION: require('./package.json').version,
+    API_URL: process.env.API_URL,
+    IMAGE_ASSETS_URL: 'https://assets.zeit.co/image/upload/front',
+    VIDEO_ASSETS_URL: 'https://assets.zeit.co/video/upload/front',
+    RAW_ASSETS_URL: 'https://assets.zeit.co/raw/upload/front',
+    ASSETS: isProd ? '/docs/static' : '/static'
+  },
+
+  webpack(config, { isServer }) {
+    if (isServer) {
+      require('./scripts/generate-site-map')()
     }
-  })
-
-  return withMDX(nextConfig)
-}
+    return config
+  }
+})
